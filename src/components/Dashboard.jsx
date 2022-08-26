2859641a855c41afc3c4c566cc3c3f3d85d5ef02
gemtunde <gemtunde@gmail.com>
2022-08-26 11:50:28 +0100

    navbar

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+import Navbar from './Navbar';
+import Analytics from './Analytics';
+import FAQ from './FAQ';
+import Earnings from './Earnings';
+import Transfers from './Transfers';
+import Profile from './Profile'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <Section>
+      <div className="navbar">
+      <Navbar />
+      </div>
+      <div className="grid">
+        <div className="row__one">
+          <Analytics />
+          <FAQ />
+        </div>
+        <div className="row__two">
+          <Earnings />
+          <Transfers />
+          <Profile />
+        </div>
+      </div>
+      </Section>
   )
 }
 
 export default Dashboard
+
+const Section = styled.section`
+  margin-left: 18vw;
+  height: 100%;
+  .navbar{
+    width: 100%;
+    border-bottom: 0.1rem solid #555;    
+   // position: fixed;
+  }
+  .grid{
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    gap:1rem;
+    margin-top: 2rem;
+
+    .row__one{
+      display: grid;
+      grid-template-columns:repeat(2, 1fr);
+      height: 50%;
+      gap: 1rem;
+    }
+    .row__two{
+      display: grid;
+      grid-template-columns:repeat(3, 1fr);
+      height: 50%;
+      gap: 1rem;
+    }
+  }
+`;
+
+
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import {BiSearch} from 'react-icons/bi'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Nav>
+      <div className="title">
+        <h4>Hi gemtunde,</h4>
+        <h1>Welcome to <span> My Taxi Dashboard </span></h1>
+      </div>
+      <div className="search">
+        <BiSearch/>
+        <input type='text' placeholder='Search' />
+      </div>
+    </Nav>
   )
 }
 
 export default Navbar
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  color: white;
+  padding: 2rem; 
+  //or try this 
+  position: sticky;
+
+    .title{
+      h1{
+        span{
+          margin-left: 0.5rem;
+          color: orange;
+          letter-spacing: 0.2rem;
+          font-family: 'Permanent Marker', cursive;
+        }
+      }
+    }
+    .search{
+      background-color: #555;
+      display: flex;
+      align-items: center;
+      padding: 1rem 8rem 1rem 1rem;
+      border-radius: 1rem;
+      gap:1rem;
+
+      svg{
+        color: orange;  
+        cursor: pointer;      
+      }
+      input{
+          background-color: transparent;
+          border: none;
+          color: orange;
+          font-family: 'Permanent Marker', cursive;
+          letter-spacing: 0.2rem;
+          &::placeholder{
+            color: orange;
+          }
+          &:focus{
+            outline: none;
+          }
+        }
+    }
+`;
